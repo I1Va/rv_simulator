2f57894ee272bebd9ee0ce9869f59eac85a0aff1
ivan_a <mai.abrutinid@gmail.com>
2026-03-22 14:28:59 +0300

    add release build

---
+++ release_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release
+cmake --build build -j 8
+# ./build/rv_simulator
+
+# ./build.sh && ./build/rv_simulator -s 1 -i examples/initial_state -f examples/final_state examples/sum.elf
